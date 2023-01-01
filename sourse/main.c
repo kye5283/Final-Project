1211f93f6ee82f5c9151874e1830cf265c433be8
kye5283 <hehe921115@gmail.com>
2023-01-01 18:09:40 +0800

    commit

--- sourse/main.c
+++ sourse/main.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include "../include/solo.h"
 #include "../include/multiplayer.h"
+
 int main(void)
 {
 	int a, b, i;
 	printf("請輸入遊玩模式，(單人模式=1,多人模式=2):");
 	scanf("%d", &i);
+	while ((i != 1) && (i != 2))
+	{
+		printf("模式輸入錯誤!!!請重新輸入\n\n");
+		printf("請輸入遊玩模式，(單人模式=1,多人模式=2):");
+		scanf("%d", &i);
+	}
+
 	switch (i)
 	{
 	case 1:
